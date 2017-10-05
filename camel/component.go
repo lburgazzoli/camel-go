5ec49202670a173f02369ac5f7e46d96dfc46a41
lburgazzoli <lburgazzoli@gmail.com>
2017-10-05 22:47:35 +0200

    updates

--- camel/component.go
+++ camel/component.go
@@ -7,3 +7,33 @@ type Component interface {
 
 	Process(Exchange *Exchange)
 }
+
+// ==========================
+//
+// DefaultComponent
+//
+// ==========================
+
+// DefaultComponent --
+type DefaultComponent struct {
+	context *Context
+}
+
+// SetContext --
+func (component *DefaultComponent) SetContext(context *Context) {
+	component.context = context
+}
+
+// Context --
+func (component *DefaultComponent) Context() *Context {
+	return component.context
+}
+
+// Status --
+func (component *DefaultComponent) Status() ServiceStatus {
+	return ServiceStatusSTARTED
+}
+
+// Process --
+func (component *DefaultComponent) Process(message string) {
+}
