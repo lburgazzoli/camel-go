ae620f6c079a2b1ac28a562275b5953cd7cbd6e8
lburgazzoli <lburgazzoli@gmail.com>
2018-06-26 12:41:29 +0200

    plugin

--- camel/route_loader_yaml_filter.go
+++ camel/route_loader_yaml_filter.go
@@ -1,6 +1,10 @@
 package camel
 
 import (
+	"fmt"
+	"os"
+	"plugin"
+
 	"github.com/mitchellh/mapstructure"
 	zlog "github.com/rs/zerolog/log"
 )
@@ -9,7 +13,8 @@ import (
 type FilterStep struct {
 	TypedStep
 
-	Ref string `yaml:"ref"`
+	Ref      string `yaml:"ref"`
+	Location string `yaml:"location"`
 }
 
 // FilterStepHandler --
@@ -23,5 +28,30 @@ func FilterStepHandler(step Step, route *RouteDefinition) (*RouteDefinition, err
 	}
 
 	zlog.Debug().Msgf("handle filter: step=<%v>, impl=<%+v>", step, impl)
+
+	if impl.Location != "" {
+		_, err := os.Stat(impl.Location)
+
+		if os.IsNotExist(err) {
+			return nil, err
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		plug, err := plugin.Open(impl.Location)
+		if err != nil {
+			return nil, fmt.Errorf("failed to open plugin %s: %v", impl.Location, err)
+		}
+
+		symbol, err := plug.Lookup(impl.Ref)
+		if err != nil {
+			return nil, fmt.Errorf("plugin %s does not export symbol \"%s\"", impl.Location, impl.Ref)
+		}
+
+		return route.Filter().Fn(symbol.(func(*Exchange) bool)), nil
+	}
+
 	return route.Filter().Ref(impl.Ref), nil
 }
