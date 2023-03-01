d02023ec0a0096bc1af9524ddc6364a1d8cd2514
Luca Burgazzoli <lburgazzoli@gmail.com>
2023-03-01 12:33:36 +0100

    cleanup

--- pkg/components/dapr/dapr_config.go
+++
@@ -1,6 +0,0 @@
-////go:build components_dapr || components_all
-
-package dapr
-
-type Config struct {
-}
--- pkg/components/http/http_config.go
+++
@@ -1,6 +0,0 @@
-////go:build components_http || components_all
-
-package http
-
-type Config struct {
-}
