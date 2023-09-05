549dad06d3d1269792bf4c0386f6f890f4bc88c5
Luca Burgazzoli <lburgazzoli@gmail.com>
2023-09-05 14:15:05 +0200

    samples

--- etc/support/mqtt/mqtt.sh
+++ etc/support/mqtt/mqtt.sh
@@ -1,3 +1,5 @@
 mosquitto_pub -h test.mosquitto.org -p 1883 -t camel-iot -m '{ "source": "sensor-1", "data": "foo" }'
 mosquitto_pub -h test.mosquitto.org -p 1883 -t camel-iot -m '{ "source": "sensor-1", "data": "foo" }' | jq .
-kubectl -n redpanda-system exec -ti redpanda-0 -c redpanda -- rpk topic consume --brokers redpanda-0.redpanda.redpanda-system.svc.cluster.local.:9093 -f json near far
+
+kubectl -n redpanda-system exec -ti redpanda-0 -c redpanda -- \
+  rpk topic consume --brokers redpanda-0.redpanda.redpanda-system.svc.cluster.local.:9093 -f json near far
